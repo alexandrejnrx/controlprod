d22f81e0249f8c6c48901cff01d1f0c6cc0c28e0
alexandrejnrx <alexandre.jnrx@gmail.com>
2026-01-31 09:12:57 -0300

    feat(loginService): separa funcao de login

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,29 +1,10 @@
 import api from '@/services/api.js'
-import { tokenService } from '@/services/tokenService.js'
-import router from '@/router/index.js'
-import { useAuthStore } from '@/stores/authStore.js'
 
-export async function login(username, password) {
-  const authStore = useAuthStore()
+export async function signIn(username, password) {
+  const response = await api.post('/auth/login', {
+    username,
+    password,
+  })
 
-  authStore.setLoading(true)
-  authStore.clearError()
-
-  try {
-    const response = await api.post('/auth/login', {
-      username,
-      password,
-    })
-
-    tokenService.saveToken(response.data.token)
-    await router.push('/home')
-
-    return { success: true }
-  } catch (error) {
-    console.error('Erro ao tentar realizar login', error)
-
-    return { success: false }
-  } finally {
-    authStore.setLoading(false)
-  }
+  return response.data
 }
